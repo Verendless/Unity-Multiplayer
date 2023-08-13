06b6fa69370ac465f53b0b137b82df65761dc9bf
Hadi Sutarma <Hadisutarma12@Gmail.com>
2023-08-13 11:03:02 +0700

    Spawning Coins

--- Assets/Scripts/Core/Coin/RespawningCoin.cs
+++ Assets/Scripts/Core/Coin/RespawningCoin.cs
@@ -1,7 +1,29 @@
+using System;
+using UnityEngine;
+
 namespace Coin
 {
     public class RespawningCoin : Coin
     {
+        public event Action<RespawningCoin> OnCollected;
+        private Vector3 previousPos;
+
+        public override void OnNetworkSpawn()
+        {
+            previousPos = transform.position;
+        }
+
+        private void Update()
+        {
+            if(!IsServer) { return; }
+            if(transform.position != previousPos)
+            {
+                Show(true);
+            }
+            previousPos = transform.position;
+
+        }
+
         public override int Collect()
         {
             if (!IsServer)
@@ -13,7 +35,14 @@ namespace Coin
             if (isCollected) return 0;
 
             isCollected = true;
+            OnCollected?.Invoke(this);
+
             return coinValue;
+        }
+
+        public void Reset()
+        {
+            isCollected = false;
         }
     }
 }
